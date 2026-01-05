6809608785816a90139daa228fb69b59f3c1090e
konkon-fox <kon-fox-kon@proton.me>
2026-01-05 16:21:35 +0900

    fix: 不要な処理を削除j

--- bbs/dat/dat.php
+++ bbs/dat/dat.php
@@ -17,12 +17,6 @@ header('Content-Type: text/plain; charset=Shift_JIS');
 header('Connection: keep-alive');
 
 // 特殊スレの場合
-if ($thread === '1000000001') {
-    $line = '過去ログの名無し<><>2001/09/09 (日) 10:46:41 ID:FAILED<>スレタイ一覧の取得に失敗しました<>スレタイ一覧の取得に失敗しました' . "\n";
-    $errorData = mb_convert_encoding($line, 'SJIS-win', 'UTF-8');
-    echo $errorData;
-    exit;
-}
 if ($thread === '1000000002') {
     $line = '過去ログの名無し<><>2001/09/09 (日) 10:46:41 ID:FAILED<>この板の過去ログ板は現在使えません<>この板の過去ログ板は現在使えません' . "\n";
     $errorData = mb_convert_encoding($line, 'SJIS-win', 'UTF-8');
@@ -36,7 +30,7 @@ $data = @file_get_contents($targetUrl);
 
 // 通信失敗
 if ($data === false) {
-    $line = '過去ログの名無し<><>2001/09/09 (日) 10:46:41 ID:FAILED<>通信に失敗しました<>通信に失敗しました' . "\n";
+    $line = '通信に失敗しました。';
     $errorData = mb_convert_encoding($line, 'SJIS-win', 'UTF-8');
     http_response_code(503);
     echo $errorData;
